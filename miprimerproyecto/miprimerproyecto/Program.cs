c5b120cd56a0c2356afcc57212bb419d8515b377
davidcastro31 <davidguille31@gmail.com>
2024-08-09 21:43:57 -0600

    Prioridad de los operadores aritmeticos (corregido)

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -10,22 +10,21 @@ namespace miprimerproyecto
     {
         static void Main(string[] args){
             Boolean comprobar = true;
-            Console.WriteLine("el valor es: {0} ",comprobar);
+            Console.WriteLine("El valor es: {0}", comprobar);
             //Prioridad de los operadores aritmeticos.
             //se pone el sufijo m para la division con decimales.
-            decimal respuesta = 5 + 8 - 2 * 5 / 4m + 1;//valor incorrecto: 14.75, valor correcto 11.5.
-            Console.WriteLine("La respuesta es: {0} ", respuesta);
+            decimal respuesta = 5 + 8 - 2 * 5 / 4m + 1; //El valor incorrecto es : 14.75, el valor correcto es: 11.5.
+            Console.WriteLine("La respuesta es: {0}", respuesta);
 
             Console.Write("Nombre: ");
             string nombre = Console.ReadLine();
 
-            Console.WriteLine("Hola {0} bienvenido ",nombre);
+            Console.WriteLine("Hola {0} bienvenido", nombre);
 
             DateTime fechaActual = new DateTime();
             fechaActual = DateTime.Now;
-            Console.WriteLine("La fecha actual es: {0} ", fechaActual);
+            Console.WriteLine("La fecha actual es: {0}", fechaActual);
             Console.WriteLine("La division de 5/4 es {0}", 5 / 4);
-
             //Pausa.
             Console.ReadLine();
         }
