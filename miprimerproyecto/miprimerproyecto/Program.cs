967459937a170689e6dbeeaa3e0093f13cbec734
davidcastro31 <davidguille31@gmail.com>
2024-08-09 21:58:38 -0600

    ejercicio de obtener la nota final de la materia de Programacion I(corregido)

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -8,14 +8,15 @@ namespace miprimerproyecto
 {
     class Program
     {
-        static void Main(string[] args){
-            //ejercicio de obtener la nota final de la materia de Programacion I.
+        static void Main(string[] args)
+        {//ejercicio de obtener la nota final de la materia de Programacion I.
             Console.Write("Lab1: ");
             double lab1 = double.Parse(Console.ReadLine()); //8
 
             Console.Write("Lab2: ");
             double lab2 = double.Parse(Console.ReadLine()); //9
 
+            Console.WriteLine("La division de 5/4 es {0}", 5 / 4);
             Console.Write("Parcial 1: ");
             double parcial1 = double.Parse(Console.ReadLine()); //7
 
@@ -26,7 +27,6 @@ namespace miprimerproyecto
 
             double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
             Console.WriteLine("La nota de C1 es: {0}", c1);
-
             //Pausa.
             Console.ReadLine();
         }
