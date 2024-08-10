034231c2d981b87e80bd92377f4c60d60e004cee
davidcastro31 <davidguille31@gmail.com>
2024-08-09 22:12:29 -0600

    ejercicio para obtener el promedio de una serie de numeros

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -9,46 +9,15 @@ namespace miprimerproyecto
     class Program
     {
         static void Main(string[] args){
-            Console.Write("Lab1: ");
-            double lab1 = double.Parse(Console.ReadLine());
-
-            Console.Write("Lab2: ");
-            double lab2 = double.Parse(Console.ReadLine());
-
-            Console.Write("Parcial1: ");
-            double parcial1 = double.Parse(Console.ReadLine());
-
-            double C1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Console.WriteLine("La nota del computo 1 es: {0}", C1);
-
-
-            Console.Write("Lab1C2: ");
-            double lab1C2 = double.Parse(Console.ReadLine());
-
-            Console.Write("Lab2C2: ");
-            double lab2C2 = double.Parse(Console.ReadLine());
-
-            Console.Write("Parcial2: ");
-            double parcial2 = double.Parse(Console.ReadLine());
-
-            double C2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Console.WriteLine("La nota del computo 2 es: {0}", C2);
-
-
-            Console.Write("Lab1C3: ");
-            double lab1C3 = double.Parse(Console.ReadLine());
-
-            Console.Write("Lab2: ");
-            double lab2C3 = double.Parse(Console.ReadLine());
-
-            Console.Write("Parcial3: ");
-            double parcial3 = double.Parse(Console.ReadLine());
-
-            double C3 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Console.WriteLine("La nota del computo 3 es: {0}", C3);
-
-            double NT = (C1 + C2 + C3) / 3;
-            Console.WriteLine("La nota final es de: {0}", NT);
+            //ejercicio obtener el promedio de una serie de numeros
+            int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
+            int suma = 0;
+            foreach (int num in serie)
+            {
+                suma += num;
+            }
+            decimal prom = suma / serie.Length;
+            Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom);
 
             //Pausa.
             Console.ReadLine();
