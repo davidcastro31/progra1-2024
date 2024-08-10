54a786ebdb21f6d3edbd419123da34609f790bdd
davidcastro31 <davidguille31@gmail.com>
2024-08-09 22:17:46 -0600

    1. if. ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido sino que le diga que es un adolescente

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -9,18 +9,21 @@ namespace miprimerproyecto
     class Program
     {
         static void Main(string[] args){
-            //ejercicio obtener el promedio de una serie de numeros
-            int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
-            int suma = 0;
-            foreach (int num in serie)
-            {
-                suma += num;
-            }
-            decimal prom = suma / serie.Length;
-            Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom);
+            //estructuras de control.
+            //1. if. ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido.
+            Console.Write("Edad: ");
+            int edad = int.Parse(Console.ReadLine());
 
-            //Pausa.
-            Console.ReadLine();
+            if (edad >= 18)
+            {
+                Console.WriteLine("Bienvenido al mundo de las reposabilidades.");
+            }
+            else
+            {
+                Console.WriteLine("Eres un adolescente, disfruta de la vida.");
+
+                //Pausa.
+                Console.ReadLine();
         }
     }
 }
