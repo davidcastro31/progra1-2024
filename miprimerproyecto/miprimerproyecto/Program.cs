abdbd23cea04861134bdec1d58bcf80266453b93
davidcastro31 <davidguille31@gmail.com>
2024-08-09 23:29:03 -0600

    Ejercicio de clasificacion de la edad usando if extendido y el ciclo mientras

--- miprimerproyecto/miprimerproyecto/Program.cs
+++ miprimerproyecto/miprimerproyecto/Program.cs
@@ -8,22 +8,40 @@ namespace miprimerproyecto
 {
     class Program
     {
-        static void Main(string[] args){
-            //estructuras de control.
-            //1. if. ejercicio. pedir al usuario la edad si es mayor de edad que le diga bienvenido.
-            Console.Write("Edad: ");
-            int edad = int.Parse(Console.ReadLine());
-
-            if (edad >= 18)
+        static void Main(string[] args) {
+            string continuar = "s";
+            while (continuar == "s")
             {
-                Console.WriteLine("Bienvenido al mundo de las reposabilidades.");
+
+                Console.Write("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
+
+                if (edad < 0)
+                {
+                    Console.WriteLine("Edad incorrecta. ");
+                }
+                else if (edad <= 2) {
+                    Console.WriteLine("Eres un bebe. ");
+                }
+                else if (edad < 12) {Console.WriteLine("Eres un niño. ");
+                }
+                else if (edad < 18) {
+                    Console.WriteLine("Eres un adolescente. ");
+                }
+                else if (edad <= 65) {
+                    Console.WriteLine("Bienvenido al mundo de las responsabilidades");
+                }
+                else if (edad <= 80) {
+                    Console.WriteLine("Eres un adulto mayor. ");
+                } else
+                {
+                    Console.WriteLine("Larga vida");
+
+                }
+
+                Console.Write("Desea continuar presione s, sino cualquier tecla.");
+                continuar = Console.ReadLine();
             }
-            else
-            {
-                Console.WriteLine("Eres un adolescente, disfruta de la vida.");
-
-                //Pausa.
-                Console.ReadLine();
         }
     }
 }
